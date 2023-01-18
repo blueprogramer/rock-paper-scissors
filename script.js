f382c695cf3b1f6ce9755fbc623ed2fae1799ab2
Mario Kovac <blueprogram036@gmail.com>
2023-01-18 14:07:50 +0100

    Added a function that plays a single game round

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ function getComputerChoice() {
     const computerChoices = ["Rock", "Paper", "Scissors"];
     return computerChoices[Math.floor(Math.random()*computerChoices.length)]
 }
+
+function playSingleRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return "It's a tie!"
+    } else if ((playerSelection === "rock" && computerSelection === "paper")
+                || (playerSelection === "paper" && computerSelection === "scissors")
+                || (playerSelection === "scissors" && computerSelection === "rock")) {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+}
