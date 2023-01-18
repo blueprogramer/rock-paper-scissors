0b6cb26254b2893fc766508c54fd513e85dd7aab
Mario Kovac <blueprogram036@gmail.com>
2023-01-18 14:31:50 +0100

    Added play game and determine winner functions, refactored the singleRound function to return integer

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let playerSelection = "Rock";
+let computerSelection = getComputerChoice();
+const gameLength = 5;
+
+
 function getComputerChoice() {
     const computerChoices = ["Rock", "Paper", "Scissors"];
     return computerChoices[Math.floor(Math.random()*computerChoices.length)]
@@ -6,13 +11,48 @@ function getComputerChoice() {
 function playSingleRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
+
+    // Return 0 when it's a tie, 1 when person wins, -1 when computer wins
     if (playerSelection === computerSelection) {
-        return "It's a tie!"
+        return 0;
     } else if ((playerSelection === "rock" && computerSelection === "paper")
                 || (playerSelection === "paper" && computerSelection === "scissors")
                 || (playerSelection === "scissors" && computerSelection === "rock")) {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+        return -1;
     } else {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+        return 1;
     }
 }
+
+function game() {
+    let playerScore = 0, computerScore = 0, i = 0;
+
+    for (i = 0; i < gameLength; i++) {
+        playerSelection = prompt("Enter your choice: Rock, Paper or Scissors.");
+        computerSelection = getComputerChoice();
+
+        if (playSingleRound(playerSelection, computerSelection) === 0) {
+            console.log("It's a tie!");
+        } else if (playSingleRound(playerSelection, computerSelection) === 1) {
+            playerScore++;
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        } else {
+            computerScore++;
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        }
+        console.log(`Current score: Player ${playerScore}-${computerScore} Computer`);
+    }
+
+    if (i === gameLength) {
+        console.log(determineWinner(playerScore, computerScore));
+    }
+}
+
+function determineWinner(playerScore, computerScore) {
+    if (playerScore === computerScore) {
+        return "FINAL RESULT: No winner! Tie!";
+    }
+    return playerScore > computerScore ? "FINAL RESULT: You win!" : "FINAL RESULT: You lose!";
+}
+
+game();
